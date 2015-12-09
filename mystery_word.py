7ff4309f917aa0253838195f42e902e6e172f440
jp <jp.park@me.com>
2015-12-09 15:13:22 -0800

    second stage

--- mystery_word.py
+++ mystery_word.py
@@ -2,9 +2,32 @@ import random
 import re
 from collections import Counter
 
-with open("/usr/share/dict/words", "r") as wordList:
-    for word in wordList:
-        x = len(word.strip())
-        if x >= 4 and x <= 6:
-            print(word.lower())
+print("Welcome to Mystery-Word! Guess the word in less than 8 tries. ")
+
+with open("/usr/share/dict/words", "r") as word_List:
+
+    def easy_game_words():
+        easy_words = []
+        for word in word_List:
+            x = len(word)
+            if x >= 4 and x <= 6:
+                easy_words.append(word.strip().lower())
+        return easy_words
+
+    def med_game_words():
+        med_words = []
+        for word in word_List:
+            x = len(word)
+            if x >= 6 and x <= 8:
+                med_words.append(word.strip().lower())
+        return med_words
+
+    def hard_game_words():
+        hard_words = []
+        for word in word_List:
+            x = len(word)
+            if x >= 8:
+                hard_words.append(word.strip().lower())
+
+
 
